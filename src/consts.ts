d07a66208793ede45065e3964ab75bcd5a1249ed
TAG-Epic <tagepicuwu@gmail.com>
2023-08-25 00:45:05 +0200

    feat: branding

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Blog';
-export const SITE_DESCRIPTION = 'Powered by the default astro blog template <3';
+export const SITE_TITLE = 'Farfrom Tech Blog';
+export const SITE_DESCRIPTION = 'Weird and cool tech findings - powered by the 🚀 default astro blog template 🚀';
