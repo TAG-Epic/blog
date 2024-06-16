89ec31b1fa314fa55b0aa338eeb61c05d2afe4a4
TAG-Epic <tagepicuwu@gmail.com>
2024-06-16 17:01:17 +0200

    fix: site bork

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,19 @@
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import BLOG_POSTS from "../posts.json";
 
 export async function get(context) {
-	const posts = await getCollection('blog');
+    const items = 
+		Object.values(BLOG_POSTS).map((post) => ({
+            title: post.title,
+            description: post.description,
+			link: `/posts/${post.slug}/`,
+            pubDate: post['pub-date'],
+		}));
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post,
-			link: `/posts/${post.slug}/`,
-		})),
+        items: items
 	});
 }
