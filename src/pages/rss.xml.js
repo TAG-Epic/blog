8082df5277f14f0e1a023707d3826148df703558
TAG-Epic <tagepicuwu@gmail.com>
2024-07-24 15:16:12 +0200

    build: update to astro 4

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 import { getReleasedPosts } from "../posts";
 
-export async function get(context) {
+export async function GET(context) {
     const items = 
 		getReleasedPosts().map((post) => ({
             title: post.title,
@@ -10,10 +10,15 @@ export async function get(context) {
 			link: `/posts/${post.slug}/`,
             pubDate: post.pubDate,
 		}));
-	return rss({
+	let rss_data = await rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
         items: items
 	});
+    return new Response(rss_data.body, {
+        headers: {
+            "Content-Type": "application/rss+xml"
+        }
+    });
 }
